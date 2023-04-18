477bb8fc0dbbd954ffb656d616d2058db0c82936
EmersonLGR <114963787+EmersonLGR@users.noreply.github.com>
2023-04-17 23:42:26 -0400

    primeras constantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
+const obtenerPerro = async () => {
+  const url = "https://dog.ceo/api/breeds/image/random";
+  const res = await fetch(url);
+  return res.json();
+};
+
+const generarNombre = (num) => {
+  const caracter = "ABCDEFGHIJKLMNOPQRSTXYZ";
+  let nombre =" ";
+  const caracterLargo = caracter.length;
+  for( let i = 0; i< num; i++){
+    nombre += caracter.charAt(Math.floor.apply(Math.random()* caracterLargo));
+  }
+  return nombre;
+
+};
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [perro, setPerro] = useState ([{ nombre: "", imagen: ""}]);
+  const [cargar, setCargar] = useState(true);
+
+  useEffect(() => {
+    obtenerPerro().then((data) => {
+      setPerro({
+        name: generarNombre(9),
+        imagen: data.message 
+      });
+      setCargar(false);
+    });
+  }, []);
+
+
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+
+<h1>
+
+</h1>
+
+
+
   )
 }
 
-export default App
+export default App;
